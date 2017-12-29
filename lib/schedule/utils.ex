21e2d858769884bdf6012b09d05ed9c0df20d893
Omkar <omkar@activesphere.com>
2017-12-29 17:17:32 +0530

    Add support for year, month and week, update tests

--- lib/schedule/utils.ex
+++ lib/schedule/utils.ex
@@ -1,22 +1,54 @@
 defmodule ExqScheduler.Schedule.Utils do
     alias Timex.Duration
+    alias __MODULE__
+
+    def get_elem(arr, index) do
+        unless arr in [nil, []] do Enum.at(arr, index) else "" end
+    end
+
+    def str_to_float(numstr) do
+        if numstr == "" do 0 else Float.parse(numstr) |> elem(0) end
+    end
 
     def to_cron(every) do
         every
     end
 
     def to_duration(timestring) do
-        datetime = String.upcase(timestring) |> String.split("D")
-        cond do
-            # If it's only date i.e. day(s) (we're not supporting week, month, year).
-            Regex.match?(~r/^\d+[dD]+$/, timestring) ->
-                Duration.parse "P#{Enum.at(datetime, 0)}D"
-            # If it's only time.
-            length(datetime) == 1 ->
-                Duration.parse "PT#{Enum.at(datetime, 0)}"
-            # If it's both date and time.
-            true ->
-                Duration.parse "P#{Enum.at(datetime,0)}DT#{Enum.at(datetime, 1)}"
+
+        # Parse week (W) syntax if present.
+        weekPart = Regex.run(~r/(\d+(\.{1}\d+)*w{1})/, timestring)
+            |> get_elem(0)
+        numWeeks = weekPart |> String.trim("w") |> str_to_float
+        timestring = String.replace(timestring, weekPart, "")
+
+        {datePart, timePart} = {
+            Regex.run(
+                ~r/(\d+(\.{1}\d+)*y)?(\d+(\.{1}\d+)*M)?(\d+(\.{1}\d+)*d)?/,
+                    timestring)
+                |> Utils.get_elem(0),
+            Regex.run(
+                ~r/(\d+(\.{1}\d+)*h)?(\d+(\.{1}\d+)*m)?(\d+(\.{1}\d+)*s)?$/,
+                    timestring)
+                |> Utils.get_elem(0)
+        }
+
+        if {datePart, timePart} == {"", ""} do
+            if numWeeks != 0 do Duration.from_weeks(numWeeks) else nil end
+        else
+            {:ok, duration} = cond do
+                # If it's only date.
+                timestring == datePart ->
+                    Duration.parse "P#{String.upcase(datePart)}"
+                # If it's only time.
+                timestring == timePart ->
+                    Duration.parse "PT#{String.upcase(timePart)}"
+                # If it's both date and time.
+                true ->
+                    Duration.parse "P#{String.upcase(datePart)}" <>
+                        "T#{String.upcase(timePart)}"
+            end
+            Duration.add(duration, Duration.from_weeks(numWeeks))
         end
     end
 end
--- test/schedule_utils_test.exs
+++ test/schedule_utils_test.exs
@@ -4,13 +4,44 @@ defmodule ScheduleUtilsTest do
     alias Timex.Duration
 
     test "to_duration(): it converts the time string to timex duration object" do
-      assert Utils.to_duration("3m") == {:ok, Duration.from_minutes(3)}
-      assert Utils.to_duration("1d") == {:ok, Duration.from_days(1)}
-      assert Utils.to_duration("1d3m") ==
-        {:ok, Duration.from_minutes(3) |> Duration.add(Duration.from_days(1))}
-      assert Utils.to_duration("2.5d3m1s") ==
-        {:ok,
-        Duration.from_minutes(3) |> Duration.add(Duration.from_days(2.5)) |>
-          Duration.add(Duration.from_seconds(1))}
+      assert Utils.to_duration("15.64s") == Duration.from_seconds(15.64)
+      assert Utils.to_duration("1s") == Duration.from_seconds(1)
+
+      assert Utils.to_duration("3.5m") == Duration.from_minutes(3.5)
+      assert Utils.to_duration("30m") == Duration.from_minutes(30)
+
+      assert Utils.to_duration("1.64h") == Duration.from_hours(1.64)
+      assert Utils.to_duration("100h") == Duration.from_hours(100)
+
+      assert Utils.to_duration("1.555d") == Duration.from_days(1.555)
+      assert Utils.to_duration("1500d") == Duration.from_days(1500)
+
+      assert Utils.to_duration("1.5d30m") ==
+        Duration.from_minutes(30) |> Duration.add(Duration.from_days(1.5))
+
+      assert Utils.to_duration("35.5w") == Duration.from_weeks(35.5)
+      assert Utils.to_duration("3w") == Duration.from_weeks(3)
+
+      assert Utils.to_duration("4.66M") == Duration.from_days(4.66 * 30)
+      assert Utils.to_duration("400M") == Duration.from_days(400 * 30)
+
+      year = (4 * 30 + 7 * 31 + 28)
+
+      assert Utils.to_duration("3.5y") == Duration.from_days(3.5 * year)
+      assert Utils.to_duration("45y") == Duration.from_days(45 * year)
+
+      assert Utils.to_duration("10w2.5d3.55m1.0s") ==
+        Duration.from_weeks(10)
+        |> Duration.add(Duration.from_days(2.5))
+        |> Duration.add(Duration.from_minutes(3.55))
+        |> Duration.add(Duration.from_seconds(1))
+
+      assert Utils.to_duration("1y2M31w2.5d3m1s") ==
+      Duration.from_days(1 * year)
+      |> Duration.add(Duration.from_days(2 * 30))
+      |> Duration.add(Duration.from_weeks(31))
+      |> Duration.add(Duration.from_days(2.5))
+      |> Duration.add(Duration.from_minutes(3))
+      |> Duration.add(Duration.from_seconds(1))
     end
   end
