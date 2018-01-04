4b6ba0d072ece1a710f8b8df12978969b14064f8
Omkar <omkar@activesphere.com>
2018-01-04 14:21:51 +0530

    Run formatter for test

--- test/scheduler_serdes_test.exs
+++ test/scheduler_serdes_test.exs
@@ -2,23 +2,24 @@ defmodule SchedulerSerdesTest do
   use ExUnit.Case, async: false
 
   setup context do
-    {:ok, redis_conn} = Redix.start_link
+    {:ok, redis_conn} = Redix.start_link()
     Redix.command(redis_conn, ['FLUSHALL'])
 
     sidekiq_path = System.cwd() |> Path.join("./sidekiq")
 
-    sidekiq_task = Task.async(fn ->
-      System.cmd("#{sidekiq_path}/setup_sidekiq", [], cd: sidekiq_path)
-    end)
+    sidekiq_task =
+      Task.async(fn ->
+        System.cmd("#{sidekiq_path}/setup_sidekiq", [], cd: sidekiq_path)
+      end)
 
     IO.puts("Wait for 5 seconds for Sidekiq to initialize.")
     :timer.sleep(5000)
 
-    on_exit context, fn ->
+    on_exit(context, fn ->
       System.cmd("#{sidekiq_path}/stop_sidekiq", [], cd: sidekiq_path)
       assert_down(sidekiq_task.pid)
       Redix.stop(redis_conn, 1000)
-    end
+    end)
   end
 
   test "it makes sure the schedule has been serialized properly" do
