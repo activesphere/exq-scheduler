e1a801c0db001dd2014b0de687977083df3c94bd
Omkar <omkar@activesphere.com>
2018-01-18 13:27:01 +0530

    Add config for dev env

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,20 @@
 use Mix.Config
+
+config :exq_scheduler, :storage_opts,
+  namespace: "exq",
+  exq_namespace: "exq"
+
+config :exq_scheduler, :server_opts,
+  timeout: 10_000,
+  prev_offset: 200_000,
+  next_offset: 1000,
+  time_zone: "Asia/Kolkata"
+
+config :exq_scheduler, :redis,
+  host: "127.0.0.1",
+  port: 6379,
+  database: 0
+
+config :exq_scheduler, :schedules,
+  schedule_cron_5m: %{ "cron" => "* * * * *", "class" => "HardWorker" },
+  schedule_cron_10m: %{ "cron" => "*/2 * * * *", "class" => "HardWorker"}
