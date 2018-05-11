127c1034fd9e69081be0135d226da3bb6eb72016
akash akya <akashh246@gmail.com>
2018-05-11 15:05:27 +0530

    Removed a test case as same is covered already
    Because of time scaling, next/previous dates given the functions might be different
    than expected, causing the test case to fail indefinitely

--- test/schedule_test.exs
+++ test/schedule_test.exs
@@ -4,38 +4,31 @@ defmodule ScheduleTest do
   import TestUtils
 
   test "check get_next_run_dates for different timezone" do
-    next_date = get_next_date("0 2 * * * America/New_York")
+    next_date = get_next_date("0 2 * * * America/New_York") # -06:00
     expected_next_date = {7, 0}
     assert expected_next_date == next_date
 
-    next_date = get_next_date("0 9 * * * Asia/Kolkata")
+    next_date = get_next_date("0 9 * * * Asia/Kolkata") # +05:30
     expected_next_date = {3, 30}
     assert expected_next_date == next_date
 
-    next_date = get_next_date("0 12 * * * Asia/Katmandu")
+    next_date = get_next_date("0 12 * * * Asia/Katmandu") # -05:45
     expected_next_date = {6, 15}
     assert expected_next_date == next_date
   end
 
-  test "correctly fetches prev and next dates when timezone specified" do
-    now_utc = Time.now()
-    schedule = build_schedule("* * * * * Asia/Kolkata")
+  test "check get_prev_run_dates for different timezone" do
+    prev_date = get_prev_date("0 2 * * * America/New_York") # -06:00
+    expected_prev_date = {7, 0}
+    assert expected_prev_date == prev_date
 
-    prev_dates = ExqScheduler.Schedule.get_previous_run_dates(schedule.cron, schedule.tz_offset)
+    prev_date = get_prev_date("0 9 * * * Asia/Kolkata") # +05:30
+    expected_prev_date = {3, 30}
+    assert expected_prev_date == prev_date
 
-    Enum.with_index(prev_dates, 1)
-    |> Enum.each(fn {prev_date, index} ->
-      before_now_1min = Timex.add(now_utc, Timex.Duration.from_minutes(-index))
-      assert Timex.between?(prev_date, before_now_1min, now_utc)
-    end)
-
-    next_dates = ExqScheduler.Schedule.get_next_run_dates(schedule.cron, schedule.tz_offset)
-
-    Enum.with_index(next_dates, 1)
-    |> Enum.each(fn {next_date, index} ->
-      after_now_1min = Timex.add(now_utc, Timex.Duration.from_minutes(index))
-      assert Timex.between?(next_date, now_utc, after_now_1min)
-    end)
+    prev_date = get_prev_date("0 12 * * * Asia/Katmandu") # +05:045
+    expected_prev_date = {6, 15}
+    assert expected_prev_date == prev_date
   end
 
   test "clock should have correct precision" do
@@ -51,6 +44,14 @@ defmodule ScheduleTest do
     schedule = build_schedule(cron)
     date =
       ExqScheduler.Schedule.get_next_run_dates(schedule.cron, schedule.tz_offset)
+      |> Enum.at(0)
+    {date.hour(), date.minute()}
+  end
+
+  defp get_prev_date(cron) do
+    schedule = build_schedule(cron)
+    date =
+      ExqScheduler.Schedule.get_previous_run_dates(schedule.cron, schedule.tz_offset)
       |> Enum.at(0)
     {date.hour(), date.minute()}
   end
