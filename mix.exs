6e432445379279ac46941d588fd396b0b52685a7
Anantha Kumaran <ananthakumaran@gmail.com>
2022-08-15 11:11:27 +0530

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExqScheduler.Mixfile do
   def project do
     [
       app: :exq_scheduler,
-      version: "1.0.0-rc3",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
